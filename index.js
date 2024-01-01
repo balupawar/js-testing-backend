3cdef8506edaef13a5a67ecbaef77fdd8b4c28e8
Balu <balupawar4615@gmail.com>
2024-01-01 12:17:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+// import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) => {
+    res.send('balupawar');
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please login at --- ---</h1>');
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send("<h2>Hello Worlds!</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
